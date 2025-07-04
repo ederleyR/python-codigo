db6961d143142fa0188880cd2d4e32f19e579b08
ederleyR <ederley123@gmail.com>
2025-07-04 08:26:55 -0500

    Delete factura.py

--- factura.py
+++
@@ -1,8 +0,0 @@
-def bonAppetit(bill, k, b):
-    total_compartido = sum(bill) - bill[k]
-    parte_anna = total_compartido // 2
-    if b == parte_anna:
-        print("Bon Appetit")
-    else:
-        print(b - parte_anna)
-        
